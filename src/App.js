d88e29249cf7391d0fdf0e73bb652b246b07888c
Abdussalam Mustapha <103737038+abdussalam-mustapha@users.noreply.github.com>
2024-07-02 14:49:18 -0700

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-function App() {
+
+const mapStyles = {
+  width: '100%',
+  height: '100%',
+};
+
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map
+        google={props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{
+          lat: 37.7749,
+          lng: -122.4194,
+        }}
+      >
+        <Marker position={{ lat: 37.7749, lng: -122.4194 }} />
+      </Map>
     </div>
   );
 }
 
-export default App;
+
+
+export default GoogleApiWrapper({
+  apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+})(App);
