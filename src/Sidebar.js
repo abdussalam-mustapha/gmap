86e79859ecfe7277562945d30582d6f6e661b48f
Abdussalam Mustapha <103737038+abdussalam-mustapha@users.noreply.github.com>
2024-07-03 09:23:30 -0700

    implement gmap

---
+++ src/Sidebar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li><a>Dashboard</a></li>
+                <li><a>Post</a></li>
+                <li><a>Create Post</a></li>
+                <li><a>Profile</a></li>
+                <li><a>Notification</a></li>
+            </ul>
+            <ul>
+                <li>Log out</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Sidebar
